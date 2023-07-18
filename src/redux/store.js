ed8ebb88519df92907c9f5812d9d4da0c661e80b
Liliana Susz <proglilsu@gmail.com>
2023-07-18 23:12:16 +0200

    Add searchTitle action wizard

--- src/redux/store.js
+++ src/redux/store.js
@@ -11,6 +11,7 @@ export const getAllColumns = (state) => state.columns;
 // action creators
 export const addColumn = payload => ({ type: 'ADD_COLUMN', payload });
 export const addCard = payload => ({ type: 'ADD_CARD', payload });
+export const searchTitle = payload => ({ type: 'UPDATE_SEARCHSTRING', payload })
 
 const reducer = (state, action) => {
     switch(action.type) {
